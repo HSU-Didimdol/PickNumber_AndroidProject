0015db39cfa9ff1970387e79f5f8e5d320892cc3
oh in seong <superohinsung@naver.com>
2023-01-28 18:29:11 +0900

    네이버 지도 연결 및 gradle 파일 편집

--- presentation/src/main/java/com/example/picknumber_androidproject/MainActivity.kt
+++ presentation/src/main/java/com/example/picknumber_androidproject/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.picknumber_androidproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.naver.maps.map.NaverMapSdk
+import com.naver.maps.map.NaverMapSdk.NaverCloudPlatformClient
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        NaverMapSdk.getInstance(this).client = NaverCloudPlatformClient("f1qm5gy16z")
     }
 }
