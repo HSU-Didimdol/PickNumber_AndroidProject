47bd7a36dd7ad5dfa6ff8a86f87068f35a4bda48
oh in seong <superohinsung@naver.com>
2023-01-29 22:04:16 +0900

    mainActivity에 지도 띄우기

--- presentation/src/main/java/com/example/picknumber_androidproject/MainActivity.kt
+++ presentation/src/main/java/com/example/picknumber_androidproject/MainActivity.kt
@@ -1,70 +1,28 @@
 package com.example.picknumber_androidproject
 
-import android.app.Activity
 import android.os.Bundle
-import androidx.annotation.NonNull
+import androidx.annotation.UiThread
+import androidx.fragment.app.FragmentActivity
 import com.naver.maps.map.*
-import com.naver.maps.map.util.FusedLocationSource
 
-
-class MainActivity : Activity(), OnMapReadyCallback {
-
-    private lateinit var mapView: MapView
-    private val LOCATION_PERMISSTION_REQUEST_CODE: Int = 1000
-    private lateinit var locationSource: FusedLocationSource // 위치를 반환하는 구현체
-    private lateinit var naverMap: NaverMap
+class MainActivity : FragmentActivity(), OnMapReadyCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
-        mapView = findViewById(R.id.Walkmap_view)
-        mapView.onCreate(savedInstanceState)
-        mapView.getMapAsync(this)
+        val fm = supportFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.map, it).commit()
+            }
 
-        locationSource = FusedLocationSource(this, LOCATION_PERMISSTION_REQUEST_CODE)
-
+        mapFragment.getMapAsync(this)
     }
 
-
-    override fun onMapReady(@NonNull naverMap: NaverMap) {
-        this.naverMap = naverMap
-        naverMap.locationSource = locationSource
-        naverMap.locationTrackingMode = LocationTrackingMode.Follow
-    }
-
-    override fun onStart() {
-        super.onStart()
-        mapView.onStart()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        mapView.onResume()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        mapView.onPause()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        mapView.onSaveInstanceState(outState)
-    }
-
-    override fun onStop() {
-        super.onStop()
-        mapView.onStop()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mapView.onDestroy()
-    }
-
-    override fun onLowMemory() {
-        super.onLowMemory()
-        mapView.onLowMemory()
+    @UiThread
+    override fun onMapReady(naverMap: NaverMap) {
+        // ...
     }
 }
